babf1407dfaaac0cd658bc8c1442b4e407709af7
Felipe Gerolomo <felipegerolomo@gmail.com>
2018-07-15 23:32:08 -0300

    Dado o conceito inicial para o feed de noticias.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FeedPage } from '../pages/feed/feed';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    FeedPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    FeedPage
+  
   ],
   providers: [
     StatusBar,
